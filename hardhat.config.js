360cd1604083088c391ee31a6b1d09aadc353952
aperisss <peris.adam@outlook.fr>
2024-06-24 14:05:45 +0000

    gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("./tasks/block-number")
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -24,4 +25,11 @@ module.exports = {
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
     },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    }
 }
